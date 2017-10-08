a56673a0e280b17e1a4510eec52ca7579161b3be
Eileen Choe <echoe@lion.lmu.edu>
2017-10-08 16:59:11 -0700

    website redesign

---
+++ map.js
@@ -0,0 +1,32 @@
+(() => {
+  var svg = d3.select("svg"),
+      width = +svg.attr("width"),
+      height = +svg.attr("height");
+
+  var projection = d3.geoPatterson()
+      .scale(153)
+      .translate([width / 2, height / 2])
+      .precision(0.1);
+
+  var path = d3.geoPath()
+      .projection(projection);
+
+  svg.append("path")
+      .datum(d3.geoGraticule10())
+      .attr("class", "graticule")
+      .attr("d", path);
+
+  d3.json("https://d3js.org/world-50m.v1.json", function(error, world) {
+    if (error) throw error;
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.feature(world, world.objects.land))
+        .attr("class", "land")
+        .attr("d", path);
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+        .attr("class", "boundary")
+        .attr("d", path);
+      });
+})();
